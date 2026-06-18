41f19c6a473c04c22ad2c55a00256fe71425e260
MarianaCos <cosmarian@gmail.com>
2026-06-18 08:18:08 -0300

    Estructura inicial de cypress y bloques listos para el equipo

---
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -0,0 +1,44 @@
+describe('Challenge Shady Meadows - QA Automation', () => {
+
+  beforeEach(() => {
+    cy.visit('https://automationintesting.online/');
+  });
+
+    // ========================================================
+  //  BLOQUE 1: CINTIA
+  // ========================================================
+
+  it('XXXXXXXXXXXXXXXXXXXXX', () => {
+    
+  });
+
+
+  // ========================================================
+  // BLOQUE 2: EMERSON
+  // ========================================================
+
+  it('XXXXXXXXXXXXXXXXXXXXX', () => {
+    
+  });
+
+});
+
+// ========================================================
+  // BLOQUE 3: MARIAN (Login Admin Panel & Contacto)
+  // ========================================================
+
+  it('Caso 1: Login de Administrador Exitoso', () => {
+    
+  });
+
+  it('Caso 2: Login de Administrador Fallido', () => {
+    
+  });
+
+  it('Caso 3: Sincronización de habitaciones creadas (BUG)', () => {
+    
+  });
+
+  it('Caso 4: Formato del campo Phone en formulario de contacto (BUG)', () => {
+    
+  });
